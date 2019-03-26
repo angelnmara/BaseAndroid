89eeebe1613b02302416e750468ceb428542352b
jose david rincon angeles <angelnmara@hotmail.com>
2019-03-26 12:23:17 -0600

    se agrega facebook login

--- app/src/main/java/com/lamarrulla/baseandroid/LoginActivity.java
+++ app/src/main/java/com/lamarrulla/baseandroid/LoginActivity.java
@@ -5,7 +5,9 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -21,6 +23,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Base64;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -30,11 +34,20 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.lamarrulla.baseandroid.implement.Acceso;
 import com.lamarrulla.baseandroid.interfaces.IAcceso;
 import com.lamarrulla.baseandroid.utils.Utils;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,6 +61,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     Context context = this;
     IAcceso iAcceso = new Acceso();
     Utils utils = new Utils();
+    CallbackManager callbackManager = CallbackManager.Factory.create();
+
     /**
      * Id to identity READ_CONTACTS permission request.
      */
@@ -75,6 +90,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Valida Hash // Si tienes problemas que no cuadre tu hash puedes descomentar esta funcion
+        //validaHash();
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
@@ -101,6 +120,29 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        /*  Facebook */
+
+        LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        //Callback registration
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(LoginActivity.this, "Usuario Logeado", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(LoginActivity.this, "Cancel", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+        /*  End Facebook */
     }
 
     private void populateAutoComplete() {
@@ -356,6 +398,29 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         protected void onCancelled() {
             mAuthTask = null;
             showProgress(false);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+        Toast.makeText(LoginActivity.this, "ActivityResult", Toast.LENGTH_SHORT).show();
+    }
+
+    private void validaHash(){
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.lamarrulla.baseandroid",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
--- app/src/main/java/com/lamarrulla/baseandroid/SplashActivity.java
+++ app/src/main/java/com/lamarrulla/baseandroid/SplashActivity.java
@@ -5,28 +5,46 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.Bundle;
 
+import com.facebook.AccessToken;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 public class SplashActivity extends Activity {
 
     private final int SPLASH_DISPLAY_LENGTH = 1000;
     private final String splash = "SplashActivity";
+    boolean isLoggedInFacebook = false;
+    Intent mainIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        /* valida si ya esta logeado con facebook */
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        isLoggedInFacebook = accessToken != null && !accessToken.isExpired();
+        /* valida si ya esta logeado con facebook */
+
         setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
                 validaToken();
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
 
     private void validaToken(){
-        Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
-        mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-        SplashActivity.this.startActivity(mainIntent);
-        SplashActivity.this.finish();
+        if(isLoggedInFacebook){
+            mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+        }else{
+            mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
+        }
     }
 }
