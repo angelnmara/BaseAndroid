d51b9a4cd047f69528cc303343962d54dcfb21df
Dave R <angelnmara@hotmail.com>
2019-04-09 23:57:03 -0500

    mensaje

--- app/src/main/java/com/lamarrulla/baseandroid/SplashActivity.java
+++ app/src/main/java/com/lamarrulla/baseandroid/SplashActivity.java
@@ -4,15 +4,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class SplashActivity extends Activity {
 
+    private static final String TAG = "SplashActivity";
     private final int SPLASH_DISPLAY_LENGTH = 1000;
     private final String splash = "SplashActivity";
     boolean isLoggedInFacebook = false;
@@ -35,6 +43,8 @@ public class SplashActivity extends Activity {
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         isLoggedInFirebase = (currentUser!=null?true:false);
+        /*genera token*/
+        generaTokenEnvioMensajesFirebase();
         /*  inicializa firebase */
 
         setContentView(R.layout.activity_splash);
@@ -56,5 +66,26 @@ public class SplashActivity extends Activity {
         }else{
             mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
         }
+    }
+
+    private void generaTokenEnvioMensajesFirebase(){
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        //String msg = getString(R.string.action_registrar, token);
+                        Log.d(TAG, token);
+                        Toast.makeText(SplashActivity.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
